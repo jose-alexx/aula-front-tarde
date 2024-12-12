c826c574af88146be03f17c50a7a9de7433a2532
jose alex <josealex41012@gmail.com>
2024-12-12 11:16:11 -0300

    enviando aula 12/12/2024

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 alert("teste")
+
+async function listarAlunos () {
+    const response = await fetch("http://localhost:3333/alunos")
+
+    console.log(response)
+    const alunos = await response.json()
+    console.log(alunos)
+    return alunos
+}
+
+// listarAlunos()
+
+async function cadastrarAluno (aluno) {
+
+    const response = await fetch("http://localhost:3333/alunos", {
+        method: "POST",
+        body: JSON.stringify(aluno),
+        headers: {"Content-Type": "application/json"}
+    })
+}
+
+const aluno = {
+    id: 3,
+    nome: "ifal",
+    email: "ifal2@gmail.com"
+}
+
+//cadastrarAluno(aluno)
+
+async function removerAluno (id) {
+    const response = await fetch("http://localhost:3333/alunos/" + id , {
+        method: "DELETE"
+    })
+}
+
+removerAluno(3)
